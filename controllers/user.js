9d9f20f8c6351da1540e05f81e81b63354be6228
Dmitry Kochetkov <d.kochetkov1@yandex.ru>
2022-11-05 19:24:16 +0400

    6st stage up

--- controllers/user.js
+++ controllers/user.js
@@ -87,37 +87,45 @@ export const createUser = (req, res) => {
 };
 
 export const updateUser = (req, res) => {
+  const { name, about } = req.body;
   User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
-    .then((user) => {
-      if (user) {
-        res.send(user);
-      } else {
-        responseNotFound(res, err.message);
-      }
-    })
-    .catch((err) => {
-      if (err.name === "ValidationError") {
-        responseBadRequestError(res, err.message);
-      } else {
-        responseServerError(res, err.message);
-      }
-    });
+  .then((user) => {
+    if (user) {
+      res.send({ data: user });
+    } else {
+      res.status(constants.HTTP_STATUS_NOT_FOUND).send({ message: 'Пользователь не найден' });
+      // responseNotFound(res, err.message);
+    }
+  })
+  .catch((err) => {
+    if (err.name === 'CastError') {
+      // res.status(constants.HTTP_STATUS_BAD_REQUEST).send({ message: 'Введены некорректные данные поиска' });
+      responseBadRequestError(res, err.message);
+    } else {
+      // res.status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка сервера' });
+      responseServerError(res, err.message);
+    }
+  });
 };
 
 export const updateAvatar = (req, res) => {
+  const { avatar } = req.body;
   User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
-    .then((user) => {
-      if (user) {
-        res.send(user);
-      } else {
-        responseNotFound(res, err.message);
-      }
-    })
-    .catch((err) => {
-      if (err.name === "ValidationError") {
-        responseBadRequestError(res, err.message);
-      } else {
-        responseServerError(res, err.message);
-      }
-    });
+  .then((user) => {
+    if (user) {
+      res.send({ data: user });
+    } else {
+      res.status(constants.HTTP_STATUS_NOT_FOUND).send({ message: 'Пользователь не найден' });
+      // responseNotFound(res, err.message);
+    }
+  })
+  .catch((err) => {
+    if (err.name === 'CastError') {
+      // res.status(constants.HTTP_STATUS_BAD_REQUEST).send({ message: 'Введены некорректные данные поиска' });
+      responseBadRequestError(res, err.message);
+    } else {
+      // res.status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка сервера' });
+      responseServerError(res, err.message);
+    }
+  });
 };

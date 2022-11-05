9504394cf839894f9b5c99e673e2dafde7c86626
Dmitry Kochetkov <d.kochetkov1@yandex.ru>
2022-11-05 20:34:17 +0400

    12st stage up

--- app.js
+++ app.js
@@ -5,19 +5,15 @@ import bodyParser from "body-parser";
 import { userRouter } from "./routes/userRouter.js";
 import { cardRouter } from "./routes/cardRouter.js";
 
-const { PORT = 3000 } = process.env;
-
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const { PORT = 3000 } = process.env;
+
 mongoose.set({ runValidators: true });
 mongoose.connect("mongodb://localhost:27017/mestodb");
 
-app.listen(PORT, () => {
-  // Если всё работает, консоль покажет, какой порт приложение слушает
-  console.log(`App listening on port ${PORT}`);
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   req.user = {
@@ -27,11 +23,16 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use("/", userRouter);
+app.use("/", cardRouter);
+
 app.all("/*", (req, res) => {
   res
     .status(constants.HTTP_STATUS_NOT_FOUND)
     .send({ message: "Такой страницы не существует" });
 });
 
-app.use("/", userRouter);
-app.use("/", cardRouter);
+app.listen(PORT, () => {
+  // Если всё работает, консоль покажет, какой порт приложение слушает
+  console.log(`App listening on port ${PORT}`);
+});

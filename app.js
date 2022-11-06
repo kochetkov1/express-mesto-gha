ba8f811083f23dea368e2b742b329ea5c0099bcb
Dmitry Kochetkov <d.kochetkov1@yandex.ru>
2022-11-06 12:23:21 +0400

    fix: 1st review

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
-import express from "express";
-import mongoose from "mongoose";
-import { constants } from "http2";
-import bodyParser from "body-parser";
-import { userRouter } from "./routes/userRouter.js";
-import { cardRouter } from "./routes/cardRouter.js";
+import express from 'express';
+import mongoose from 'mongoose';
+import { constants } from 'http2';
+import bodyParser from 'body-parser';
+import { userRouter } from './routes/userRouter.js';
+import { cardRouter } from './routes/cardRouter.js';
 
 const app = express();
 
 const { PORT = 3000 } = process.env;
 
 mongoose.set({ runValidators: true });
-mongoose.connect("mongodb://localhost:27017/mestodb");
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   req.user = {
-    _id: "63652cbed4f346d9c2d88b8d",
+    _id: '63652cbed4f346d9c2d88b8d',
   };
 
   next();
 });
 
-app.use("/", userRouter);
-app.use("/", cardRouter);
+app.use('/', userRouter);
+app.use('/', cardRouter);
 
-app.all("/*", (req, res) => {
+app.all('/*', (req, res) => {
   res
     .status(constants.HTTP_STATUS_NOT_FOUND)
-    .send({ message: "Такой страницы не существует" });
+    .send({ message: 'Такой страницы не существует' });
 });
 
 app.listen(PORT, () => {
